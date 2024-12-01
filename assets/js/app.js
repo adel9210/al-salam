f5ce9c2982a290758765376db6dcba29d8596ce7
adelsadek <adel.sadek@ihorizons.com>
2024-12-01 14:44:49 +0200

    - responsive

--- assets/js/app.js
+++ assets/js/app.js
@@ -34,6 +34,33 @@ jQuery(document).ready(function ($) {
       },
     },
   });
+
+  const servicesSwiper = new Swiper(".our-services__swiper", {
+    spaceBetween: 47,
+    clickable: true,
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 0,
+      },
+      // when window width is >= 480px
+      480: {
+        slidesPerView: 1,
+        spaceBetween: 0,
+      },
+      // when window width is >= 768px
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      // when window width is >= 991px
+      991: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+      },
+    },
+  });
   $('.loader').fadeOut('slow');
 
 
